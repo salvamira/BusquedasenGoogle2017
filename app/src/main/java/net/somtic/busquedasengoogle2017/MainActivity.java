bdd18d63a351432e6ffcdf3020899a9648ce6d30
Salva Mira <smira@iesperemaria.com>
2017-01-04 18:28:27 +0100

    Initial commit

---
+++ app/src/main/java/net/somtic/busquedasengoogle2017/MainActivity.java
@@ -0,0 +1,73 @@
+package net.somtic.busquedasengoogle2017;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText entrada;
+    private TextView salida;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        entrada = (EditText) findViewById(R.id.EditText01);
+        salida = (TextView) findViewById(R.id.TextView01);
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.
+                Builder().permitNetwork().build());
+    }
+
+    public void buscar(View view){
+        try {
+            String palabras = entrada.getText().toString();
+            String resultado = resultadosGoogle(palabras);
+            salida.append(palabras + "--" + resultado + "\n");
+        } catch (Exception e) {
+            salida.append("Error al conectar\n");
+            Log.e("HTTP", e.getMessage(), e);
+        }
+    }
+
+    String resultadosGoogle(String palabras) throws Exception {
+        String pagina = "", devuelve = "";
+        URL url = new URL("https://www.google.es/search?hl=es&q=\""
+                + URLEncoder.encode(palabras, "UTF-8") + "\"");
+        HttpURLConnection conexion =
+                (HttpURLConnection)url.openConnection();
+        conexion.setRequestProperty("User-Agent",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
+        if (conexion.getResponseCode()==HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(conexion.getInputStream()));
+            String linea = reader.readLine();
+            while (linea != null) {
+                pagina += linea;
+                linea = reader.readLine();
+            }
+            reader.close();
+            int ini = pagina.indexOf("Aproximadamente");
+            if (ini != -1) {
+                int fin = pagina.indexOf(" ", ini + 16);
+                devuelve = pagina.substring(ini + 16, fin);
+            } else {
+                devuelve = "no encontrado";
+            }
+        } else {
+            salida.append("ERROR: " +
+                    conexion.getResponseMessage() + "\n");
+        }
+        conexion.disconnect();
+        return devuelve;
+    }
+}
